64f292524544e3b32bb92a3d68ef2fea2994159b
Jacopo De Rosa <j.derosa@rbw-cgi.it>
2024-09-20 14:18:47 +0200

    Add animations

---
+++ Assets/Scripts/AI/NavmeshAnimatorUpdater.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI
+{
+    public class NavmeshAnimatorUpdater : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _agent;
+       
+    }
+}
